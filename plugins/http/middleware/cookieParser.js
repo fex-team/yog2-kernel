fda4e2099201e524c52de257532c6827681ae723
hefangshi <hefangshi@baidu.com>
2014-12-05 16:02:44 +0800

    fix cookieParser config

--- plugins/http/middleware/cookieParser.js
+++ plugins/http/middleware/cookieParser.js
@@ -1,6 +1,6 @@
 var cookieParser = require('cookie-parser');
 module.exports.cookieParser = function(app, conf){
     return function(){
-        app.use(cookieParser(conf));
+        app.use(cookieParser(conf.secret, conf));
     };
 };
