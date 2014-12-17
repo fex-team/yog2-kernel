dfce8b8bbcea7f49e3c6b90a3565b9fccdd25a6d
hefangshi <hefangshi@gmail.com>
2014-12-17 10:56:31 +0800

    add fatal log

--- plugins/http/middleware/error.js
+++ plugins/http/middleware/error.js
@@ -1,14 +1,19 @@
 var debuglog = require('debuglog')('yog/plugins');
 
-module.exports.error = function(app, conf){
+module.exports.error = ['log', function(app, conf){
     return function(){
         if (!yog.DEBUG){
             app.use(function(error, req, res, next){
+                yog.log.fatal(error);
                 res.status(500);
                 res.send('500: Internal Server Error');
             });
         }else{
             debuglog('start debug mode for error page'.yellow);
+            app.use(function(error, req, res, next){
+                yog.log.fatal(error);
+                next(error);
+            });
         }
     };
-};
+}];
