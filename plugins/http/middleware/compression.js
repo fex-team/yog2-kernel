6394b1e26a58c20bb59c401539e38087913a94a4
hefangshi <hefangshi@gmail.com>
2014-12-29 14:56:28 +0800

    release 0.0.22
    在有Bigpipe组件时，不进行压缩

--- plugins/http/middleware/compression.js
+++ plugins/http/middleware/compression.js
@@ -1,5 +1,23 @@
+var compressible = require('compressible');
+
 module.exports.compression = function(app, conf){
     return function(){
         app.use(require('compression')(conf));
     };
+};
+
+module.exports.compression.defaultConf = {
+    filter: function(req, res){
+        if (res.bigpipe && Object.keys(res.bigpipe.sources).length !== 0){
+            return false;
+        }
+
+        var type = res.getHeader('Content-Type');
+
+        if (type === undefined || !compressible(type)) {
+            return false;
+        }
+
+        return true;
+    }
 };
