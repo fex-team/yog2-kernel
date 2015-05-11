ab278caa4941e2efb44f4a11f477e9bd03e0bb3e
hefangshi <hefangshi@gmail.com>
2015-05-11 16:14:21 +0800

    [enhancement] add custom handler for notFound and error page

--- plugins/http/middleware/error.js
+++ plugins/http/middleware/error.js
@@ -1,19 +1,25 @@
 var debuglog = require('debuglog')('yog/plugins');
 
-module.exports.error = ['log', function(app, conf){
-    return function(){
-        if (!yog.DEBUG){
-            app.use(function(error, req, res, next){
-                yog.log.fatal(error);
-                res.status(500);
-                res.send('500: Internal Server Error');
-            });
-        }else{
-            debuglog('start debug mode for error page'.yellow);
-            app.use(function(error, req, res, next){
-                yog.log.fatal(error);
-                next(error);
-            });
-        }
-    };
-}];
+module.exports.error = ['log',
+    function (app, conf) {
+        return function () {
+            if (!yog.DEBUG) {
+                app.use(conf.handler);
+            } else {
+                debuglog('start debug mode for error page'.yellow);
+                app.use(function (error, req, res, next) {
+                    yog.log.fatal(error);
+                    next(error);
+                });
+            }
+        };
+    }
+];
+
+module.exports.error.defaultConf = {
+    handler: function (error, req, res, next) {
+        yog.log.fatal(error);
+        res.status(500);
+        res.send('500: Internal Server Error');
+    }
+};
--- plugins/http/middleware/notFound.js
+++ plugins/http/middleware/notFound.js
@@ -1,14 +1,18 @@
 var debuglog = require('debuglog')('yog/plugins');
 
-module.exports.notFound = function(app, conf){
-    return function(){
-        if (!yog.DEBUG){
-            app.use(function(req, res){
-                res.status(404);
-                res.send('404: Page not Found');
-            });            
-        }else{
+module.exports.notFound = function (app, conf) {
+    return function () {
+        if (!yog.DEBUG) {
+            app.use(conf.handler);
+        } else {
             debuglog('start debug mode for not found page'.yellow);
         }
     };
+};
+
+module.exports.notFound.defaultConf = {
+    handler: function (req, res, next) {
+        res.status(404);
+        res.send('404: Page not Found');
+    }
 };
